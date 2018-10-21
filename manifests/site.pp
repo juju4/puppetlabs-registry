2831a1abb5c01ba540bc684cec6f8b6ceba07aa7
juju4 <juju4@users.noreply.github.com>
2018-10-21 16:12:10 -0400

    add registry_key

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,7 @@ registry_key { 'HKCR\\Software\\Vendor': ensure => present }
 Registry_key { ensure => present }
 Registry_value { ensure => present, data => 'Puppet Default Data' }
 
+registry_key { 'HKCR\\htafile\\shell\\open\\command': ensure => present }
 registry_value { "HKCR\\htafile\\shell\\open\\command":
   path       => "HKCR\\htafile\\shell\\open\\command",
   ensure     => present,
