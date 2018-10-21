82be382e3e83a92b6ee9fc5cd29f8c2e82274f84
juju4 <juju4@users.noreply.github.com>
2018-10-21 14:46:37 -0400

    test another HKCR entry

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,11 @@
 registry_key { 'HKCR\\Software\\Vendor': ensure => present }
 Registry_key { ensure => present }
 Registry_value { ensure => present, data => 'Puppet Default Data' }
+
+registry_value { "HKCR\htafile\shell\open\command":
+  path       => "HKCR\htafile\shell\open\command",
+  ensure     => present,
+  value      => '(default)',
+  type       => string,
+  data       => '%windir%\system32\notepad.exe',
+}
