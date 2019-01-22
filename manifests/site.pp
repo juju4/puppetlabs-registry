65b5319ec0dd220a7f9316e4ec18980ecb22f865
juju4 <juju4@users.noreply.github.com>
2019-01-22 18:25:04 -0500

    s/registry_value/registry::value/ + arguments

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,7 @@ Registry_value { ensure => present, data => 'Puppet Default Data' }
 
 registry_key { 'HKCR\\htafile\\shell\\open\\command': ensure => present }
 registry::value { "HKCR\\htafile\\shell\\open\\command":
-  path       => "HKCR\\htafile\\shell\\open\\command",
-  ensure     => present,
+  key => "HKCR\\htafile\\shell\\open\\command",
   type       => string,
   value      => '(default)',
   data       => '%windir%\system32\notepad.exe',
